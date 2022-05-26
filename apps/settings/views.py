876bb7cbc5a711461d15f867a26faaa548ecd72b
AbrorIT <sultanovabror578@gmail.com>
2022-05-26 17:17:58 +0600

    public

--- apps/settings/views.py
+++ apps/settings/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from apps.settings.models import Settings
+from apps.posts.models import Post
 
 def index(request):
     home = Settings.objects.latest('id')
+    posts = Post.objects.all()
     context = {
-        'homes' : home
+        'homes' : home,
+        'posts' : posts,
     }
     return render(request, 'index.html', context)
