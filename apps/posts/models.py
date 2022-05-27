3c30d35a78f08b1835944bb9ba7877e128694756
AbrorIT <sultanovabror578@gmail.com>
2022-05-27 22:30:02 +0600

    post

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -6,13 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     post_image = models.ImageField(upload_to = 'post_image/', blank = True, null = True)
-    # NEWS_CHOICES = [
-    #     ('Политика', 'Политика'),
-    #     ('Бизнес', 'Бизнес'),
-    #     ('Спорт', 'Спорт'),
-    # ]
-    # news = models.CharField(choices='NEWS_CHOICES', default='Политика', max_length=255)
-
+  
     def __str__(self):
         return self.title
 
