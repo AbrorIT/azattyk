2539ea9c521f56cf444d10ae7b3227959e163729
AbrorIT <sultanovabror578@gmail.com>
2022-05-31 17:51:05 +0600

    category

--- apps/categories/views.py
+++ apps/categories/views.py
@@ -1,3 +1,14 @@
+from unicodedata import category
 from django.shortcuts import render
+from apps.categories.models import Category
 
 # Create your views here.
+def Category(request, id):
+    category = Category.objects.all()
+    context = {
+        'category' : category,
+    }
+    return render(request, 'categories.html', context)
+    
+
+
