4164e2a77e494d92e359a54208adf67b7343bea9
AbrorIT <sultanovabror578@gmail.com>
2022-06-04 17:20:12 +0600

    update models

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from apps.posts.models import Post
+from apps.categories.models import Category
 
 # Create your views here.
 
 def Publication(request, id):
     post = Post.objects.get(id = id)
+    categories = Category.objects.all()
     context = {
         'post' : post,
+        'categories' : categories,
     }
     return render(request, 'single.html', context)
--- apps/settings/models.py
+++ apps/settings/models.py
@@ -7,6 +7,7 @@ class Settings(models.Model):
     facebook = models.CharField(max_length=255)
     twitter = models.CharField(max_length=255)
     instagram = models.CharField(max_length=255)
+    linkedin = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title
