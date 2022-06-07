39a79079bbd31c951a159a9da58f2a92d77f7c4b
AbrorIT <sultanovabror578@gmail.com>
2022-06-07 15:32:33 +0600

    search-views

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from apps.posts.models import Post
 from apps.categories.models import Category
+from apps.settings.models import Settings
+from django.db.models import Q
+
 
 # Create your views here.
 
@@ -12,3 +15,15 @@ def Publication(request, id):
         'categories' : categories,
     }
     return render(request, 'single.html', context)
+
+def post_search(request):
+    posts = Post.objects.all()
+    qury_obj = request.GET.get('key')
+    home = Settings.objects.latest('id')
+    if qury_obj:
+        posts = Post.objects.filter(Q(title__icontains = qury_obj))
+    context = {
+        'home' : home, 
+        'posts' : posts,
+    }
+    return render(request, 'index.html', context)
